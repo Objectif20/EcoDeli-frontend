b8597e8572fbbd6250c4ceb7f8b8b50e3bdf6356
Rémy <remy.thibaut2005@gmail.com>
2025-04-22 21:51:11 +0200

    Correction des restrictions de routes par rôles

--- src/components/private-profileRoutes.tsx
+++ src/components/private-profileRoutes.tsx
@@ -1,49 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAccessToken } from '../api/auth.api';
 import { AppDispatch } from '../redux/store';
 import { UserApi } from '@/api/user.api';
 import { RootState } from '../redux/store';
 
 interface PrivateProfileRoutesProps {
-  requiredProfile: string;
-  requireAuth?: boolean;
+  requiredProfiles: string[];
 }
 
-const PrivateProfileRoutes: React.FC<PrivateProfileRoutesProps> = ({ requiredProfile, requireAuth = true }) => {
+const PrivateProfileRoutes: React.FC<PrivateProfileRoutesProps> = ({ requiredProfiles }) => {
   const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const user = useSelector((state: RootState) => state.user.user);
-
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     const fetchData = async () => {
-      if (requireAuth) {
-        const response = await getAccessToken();
-
-        if (response.accessToken) {
-          setIsAuthenticated(true);
-          dispatch(UserApi.getUserData()).finally(() => setLoading(false));
-        } else {
-          setIsAuthenticated(false);
-          setLoading(false);
-        }
-      } else {
-        dispatch(UserApi.getUserData()).finally(() => setLoading(false));
-      }
+      await dispatch(UserApi.getUserData()).finally(() => setLoading(false));
     };
-
     fetchData();
-  }, [dispatch, requireAuth]);
+  }, [dispatch]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  if (loading) return <div>Loading...</div>;
 
-  if (requireAuth && (!isAuthenticated || !user?.profile.includes(requiredProfile))) {
-    return <Navigate to="/auth/login" />;
+  const hasAccess = requiredProfiles.some(profile => user?.profile.includes(profile));
+
+  if (!hasAccess) {
+    console.error('Access denied: insufficient permissions');
+    return null;
   }
 
   return <Outlet />;
---
+++ src/pages/features/deliveries/history-client.tsx
@@ -0,0 +1,10 @@
+export default function HistoryDeliveriesClientPage() 
+
+{
+  return (
+    <div>
+      <h1>History Deliveries Client</h1>
+      <p>This is the history deliveries client page.</p>
+    </div>
+  );
+}

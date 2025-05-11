2a202d20ab1715cdaab24cbb73bd25f39f8ba892
Rémy <remy.thibaut2005@gmail.com>
2025-05-11 22:14:36 +0200

    Correction pour build

--- src/components/sidebar/nav-language.tsx
+++ src/components/sidebar/nav-language.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { ChevronsUpDown, Globe } from "lucide-react"
+import { ChevronsUpDown } from "lucide-react"
 import {
   DropdownMenu,
   DropdownMenuContent,
